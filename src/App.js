e3eb3d3d19d4aaf26a368e1273bd65a15026b498
Hanmanthu Ramakrishna <hanmanthuramakrishna234@gmail.com>
2024-11-22 22:37:16 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import BookList from './components/BookList';
-import BookForm from './components/BookForm.jsx';
-import BookDetail from './components/BookDetail.jsx';
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<BookList />} />
-        <Route path="/add" element={<BookForm />} />
-        <Route path="/edit/:id" element={<BookForm />} />
-        <Route path="/book/:id" element={<BookDetail />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
-
 export default App;
